f567bba35cd881c90c18645b7e29c455a8e524ba
joaomarri <joaomarri@gmail.com>
2015-03-27 22:59:13 -0300

    proxy and port configurable

--- src/main/java/com/component/rest/proxy/RestProxyTemplate.java
+++ src/main/java/com/component/rest/proxy/RestProxyTemplate.java
@@ -5,29 +5,34 @@ import java.net.Proxy;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
+@PropertySource("classpath:config.properties") 
 public final class RestProxyTemplate {
  
     private RestTemplate restTemplate;
  
+    @Value("${proxy.host}")
     String host;
    
+    @Value("${proxy.port}")
     String port;
  
     @PostConstruct
     public void init(){
         int portNr = -1;
         try {
-            portNr = Integer.parseInt("8080");
+            portNr = Integer.parseInt(port);
         } catch (NumberFormatException e) {
            
         }
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        InetSocketAddress address = new InetSocketAddress("websense.test.com",portNr);
+        InetSocketAddress address = new InetSocketAddress(host, portNr);
         Proxy proxy = new Proxy(Proxy.Type.HTTP,address);
         factory.setProxy(proxy);
  
